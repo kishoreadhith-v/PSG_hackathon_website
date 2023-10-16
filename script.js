327dd2e0c6792fb50632624d68b3c21751f35cef
Kishoreadhith V <117294726+kishoreadhith-v@users.noreply.github.com>
2023-10-16 16:29:12 +0000

    nav animation done

--- script.js
+++ script.js
@@ -2,3 +2,21 @@ const regButton = document.querySelector('.reg');
 regButton.addEventListener('click', function() {
   alert('You have registered for the event!');
 });
+
+const movingText = document.querySelector('.moving-text');
+const topText = document.querySelector('.logo-text-top');
+const bottomText = document.querySelector('.logo-text-bottom');
+
+document.addEventListener('mousemove', (e) => {
+    const mouseX = e.clientX;
+    const windowWidth = window.innerWidth;
+    const moveRange = 100; // Adjust the range of movement
+
+    // Calculate the horizontal movement for both text elements
+    const moveTop = ((mouseX / windowWidth) - 0.5) * moveRange;
+    const moveBottom = -moveTop;
+
+    // Apply the horizontal movement to the text elements
+    topText.style.transform = `translateX(${moveTop}px)`;
+    bottomText.style.transform = `translateX(${moveBottom}px)`;
+});
